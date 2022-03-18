6ab8358b6fbd800eb5344352a673ec08556048c8
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2022-03-18 08:07:57 +0100

    jar

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * Description: computes all multiples
+ * of 3 or 5 below 1024 and prints the sum.
+ * Return: Always 0
  */
 int main(void)
 {
+	int i, sum;
 
+	sum = 0;
+	for (i = 3; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
 	return (0);
 }
