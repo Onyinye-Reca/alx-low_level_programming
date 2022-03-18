95c2b45ea1b10e2ae551ce34ab75ae3e41369845
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2022-03-18 04:49:39 +0100

    bow

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,31 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_triangle - a function that prints a triangle
- * @size: size of the triangle
- * Return: triangle of '#'s
+ * print_triangle - prints a triangle
+ *
+ * @size: the size of the triangle
+ *
+ * Return: void
  */
 void print_triangle(int size)
 {
-	int sp, ro, tr;
+	int l, b;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
+
+	for (l = 1; l <= size; l++)
 	{
-		for (ro = 0; ro <= (size - 1); ro++)
+		for (b = 1; b <= size; b++)
 		{
-			for (sp = 0; sp < (size - 1) - ro; sp++)
-			{
+			if (b < size - l + 1)
 				_putchar(' ');
-			}
-			for (tr = 0; tr <= ro; tr++)
-			{
+			else
 				_putchar('#');
-			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
