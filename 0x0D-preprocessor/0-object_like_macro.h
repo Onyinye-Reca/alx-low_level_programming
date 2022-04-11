a0f1950ae545904be889870310b1d336f72a0620
Onyinye-Reca <rebeccaobiajuru@gmail.com>
2022-04-11 23:39:57 +0000

    tired

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,3 @@
-
-4 lines (4 sloc)  81 Bytes 
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef SIZE
 #define SIZE 1024
 #endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-FUNCTION_LIKE_MACRO_H87
-#define ABS(x) ((x) < (0) ? -(x) : (x))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
